52b91a196e4697bf2482c1f2fdcb1ff1392ae595
Miriam Avranovits <m548532941@gmail.com>
2024-12-05 15:33:54 +0200

    Add Demo5 and other updates

--- Demo_4 _microservice/Repository/usersWS.js
+++ Demo_4 _microservice/Repository/usersWS.js
@@ -5,10 +5,9 @@ const API_TASKS="https://jsonplaceholder.typicode.com/todos";
 
 
 const getUserEmail = async(username)=>{
-    const url = `${API_EMAIL}?username=${username}`; // Ensure this URL is correct
+    const url = `${API_EMAIL}?username=${username}`; 
     const response = await axios.get(url);
     return response.data[0];
-//    return axios.get(`${API_EMAIL}/${username}`)
 };
 
 const getUserTask =async (username)=>{
@@ -18,13 +17,3 @@ const getUserTask =async (username)=>{
 }
 module.exports = { getUserEmail,getUserTask };
 
-// try {
-//     const response = await axios.get(url);
-//     if (response.data.length === 0) {
-//         throw new Error(`User with username ${username} not found`);
-//     }
-//     return response.data[0]; // Assuming the API returns an array of users
-// } catch (error) {
-//     console.error('Error fetching user email:', error);
-//     throw error;
-// }
